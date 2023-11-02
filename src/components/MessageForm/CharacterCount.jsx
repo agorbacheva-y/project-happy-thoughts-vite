92ee721bca43567ac6aec4fc693b7177b424e293
Michael Holm <michael@weblovers.se>
2023-11-03 00:41:40 +0100

    Publish new thought and letter count

--- src/components/MessageForm/CharacterCount.jsx
+++ src/components/MessageForm/CharacterCount.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import './CharacterCount.css'
 
-const CharacterCount = () => {
+
+const CharacterCount = ({letterCount}) => {
    /*
   Count characters.
   Validation of some form?
   */
+ const isTooLong = (letterCount > 140)
   return (
-    <div>
-      
+    <div className="characterCountContainer">
+      <div className="letterCountText">{isTooLong ? 'Message is too long' : ''}</div>
+      <div className={`letterCount ${isTooLong ? 'warning' : ''}`}>{letterCount}/140</div>
     </div>
   );
 };
--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
 import "./MessageForm.css";
+import CharacterCount from "./CharacterCount";
 
-const MessageForm = () => {
+
+const MessageForm = ({addNewThought}) => {
   /* 
   form to add thought
   input validation
   state: newThought, characterCount?  
-  
   */
+
+  const emptyThought = {
+    message: ""
+  }
+
+  const [newThought, setNewThought] = useState(emptyThought)
+
+  const handleSubmit = () => {
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "message": newThought.message })
+    })
+      .then((res) => res.json())
+      .then((newThought) => {
+        addNewThought(newThought)
+        setNewThought(emptyThought)
+      })
+  }
+
+  const saveMessage = (e) => {
+    const message = e.target.value
+    setNewThought((values) => ({...values, message}))
+  }
+
   return (
     <div className="formContainer">
       <div className="inputBox">
         <h2>What is making you happy right now?</h2>
-        <textarea 
-          className="messageInput" 
-          type="text" 
+        <textarea
+          className="messageInput"
+          value={newThought.message}
+          type="text"
           placeholder="'If music be the food of love, play on.' - William Shakespeare"
+          onChange={saveMessage}
         >
         </textarea>
-        <button>❤️ Send Happy Thought ❤️</button>
+        <CharacterCount letterCount={newThought.message.length} />
+        <button onClick={handleSubmit}>❤️ Send Happy Thought ❤️</button>
       </div>
     </div>
   );
